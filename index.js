126e92e9db2347a46903dfbebde91ad679941bd4
Claudia Scarbrough <rubyharrts@gmail.com>
2021-10-07 17:46:19 -0500

    finished

--- index.js
+++ index.js
@@ -135,8 +135,9 @@ Use the getReviewByIndex function below to do the following:
 
 
 function getReviewByIndex(array, number) {
-  /*Your code here*/
+  return `${array[number].name} gave the restaurant a ${array[number].rating} star review, and their feedback was: ${array[number].feedback}`;
 }
+console.log(getReviewByIndex(reviews, 4));
 
 
   
@@ -153,9 +154,11 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(array) {
+  return `${array[array.length-1].name} gave the restaurant a ${array[array.length-1].rating} star review, and their feedback was: ${array[array.length-1].feedback}`;
 } 
+
+console.log(getLastReview(reviews));
 
 
 
